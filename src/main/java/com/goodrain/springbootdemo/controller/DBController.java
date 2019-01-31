6caab0aea219ebc6272ac7ce577821c2da935e46
GLYASAI <chaoschunqfeijg@163.com>
2019-01-31 14:44:33 +0800

    [REV] Enabling CORS

--- src/main/java/com/goodrain/springbootdemo/controller/DBController.java
+++ src/main/java/com/goodrain/springbootdemo/controller/DBController.java
@@ -5,6 +5,7 @@ import com.goodrain.springbootdemo.util.DBConnPool;
 import com.goodrain.springbootdemo.vo.RestResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class DBController {
     public DBInfoService dbInfoService;
 
     @GetMapping("is-connected")
+    @CrossOrigin(origins = "*")
     public RestResponse<Boolean> isConnected() throws SQLException {
         DBConnPool dcp = DBConnPool.getInstance();
         Connection conn = dcp.getConnection();
@@ -34,6 +36,7 @@ public class DBController {
     }
 
     @GetMapping("list-tables")
+    @CrossOrigin(origins = "*")
     public RestResponse<List<String>> listTables() throws SQLException {
         List<String> list = dbInfoService.listTables();
 
